3b04d80783058d63757683b39b87c0ac76fd777a
ChrisChrisLoLo <sporewoh@gmail.com>
2019-02-23 23:06:04 -0700

    Begun adding bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,34 +1,27 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
-import "./App.css";
+//import "./App.css";
+import NavbarComp from "./components/NavbarComp.js"
+import { Container, Row, Col } from "reactstrap"
+
 
 class App extends Component {
   render() {
+    console.log(NavbarComp);
     return (
-      <div className="App">
+      <div>
         <head>
-          {/*Meta tags and bootstrap cdn */}
-          <meta charset="utf-werqrwEQW8" />
-          <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-          <script
-            src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
-            integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
-            crossorigin="anonymous"
-          />
           <title>Hello, world!</title>
         </head>
         <body>
-          <h1>Hello, world!</h1>
-          <script
-            src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
-            integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
-            crossorigin="anonymous"
-          />
-          <script
-            src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
-            integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
-            crossorigin="anonymous"
-          />
+          <NavbarComp />
+          <Container>
+            <Row>
+              <Col>
+                <h1>UCourse</h1>
+              </Col>
+            </Row>
+          </Container>
         </body>
       </div >
     );
