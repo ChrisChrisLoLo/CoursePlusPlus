371bbf70c367fcf405eb0cf6d0ac9e3bd749d26f
ChrisChrisLoLo <sporewoh@gmail.com>
2019-03-06 22:27:51 -0700

    Trying to add page buttons. Need to figure out how to access router history from this level of component.

--- frontend/src/components/search/SearchResults.js
+++ frontend/src/components/search/SearchResults.js
@@ -1,10 +1,15 @@
 import React from "react";
 import {
+	Button,
 	Row,
 	Col,
 	Container,
 } from 'reactstrap';
+import {
+	withRouter,
+} from 'react-router-dom';
 
+import PropTypes from "prop-types";
 import ResultItem from "./ResultItem"
 
 export default class SearchResults extends React.Component {
@@ -12,14 +17,32 @@ export default class SearchResults extends React.Component {
 		super(props);
 	}
 
+	static propTypes = {
+		match: PropTypes.object.isRequired,
+		location: PropTypes.object.isRequired,
+		history: PropTypes.object.isRequired
+	};
+
 	componentDidMount() {
 		//Scroll to top whenever more results are loaded.
 		//window.scrollTo(0, 0)
 
 	}
 
+	//Take the pagination URL from the API and push the given page to the react router history.
+	calcPaginationURL(paginationURL) {
+		console.log(this.props);
+		if (!paginationURL) return;
+
+		let searchParams = this.props.search || "?";
+		let curURL = this.props.location + searchParams;
+
+		let queryRegExp = new RegExp("(page=)[0-9]+");
+		let pageQuery = paginationURL.match(queryRegExp);
+		this.props.history.push(curURL + pageQuery);
+	}
+
 	render() {
-		console.log(this.props.courseListData)
 		//console.log(this.props.courseListData.next)
 		// const resultsList = this.props.courseListData.results.map((course) =>
 		// 	<ResultItem />
@@ -31,7 +54,7 @@ export default class SearchResults extends React.Component {
 			results = data.results;
 
 			//If empty
-			if (results === undefined || results.length == 0) {
+			if (results === undefined || results.length === 0) {
 				output = <h3>No Results Found.</h3>
 			}
 			else {
@@ -43,7 +66,6 @@ export default class SearchResults extends React.Component {
 		else {
 			output = <h3>Loading...</h3>;
 		}
-		console.log(results)
 		return (
 			<div>
 				<Container fluid={true}>
@@ -56,9 +78,26 @@ export default class SearchResults extends React.Component {
 							</Col>
 						</Row>
 					}
+
 					{output}
+
+					{data &&
+						<Row className="justify-content-between">
+							<Col sm={{ size: 'auto' }}>
+								{data.previous &&
+									<Button color="primary" onClick={this.calcPaginationURL(data.prev)}>Prev</Button>
+								}
+							</Col>
+							<Col sm={{ size: 'auto' }}>
+								{data.next &&
+									<Button color="primary" onClick={this.calcPaginationURL(data.next)}>Next</Button>
+								}
+							</Col>
+						</Row>
+					}
 				</Container>
 			</div>
 		);
 	}
 }
+
