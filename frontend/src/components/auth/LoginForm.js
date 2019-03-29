3dd44106459f7b28f7e97c7e8d7591c83252b5c9
ChrisChrisLoLo <sporewoh@gmail.com>
2019-03-29 11:32:03 -0700

    added handlers

--- frontend/src/components/auth/LoginForm.js
+++ frontend/src/components/auth/LoginForm.js
@@ -10,8 +10,31 @@ import {
     Input,
 } from 'reactstrap';
 import axios from "axios";
+import { targetPropType } from "reactstrap/lib/utils";
 
 export default class LoginForm extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {username:"", password:""}
+
+        this.attemptLogin = this.attemptLogin.bind(this);
+        this.handleUsernameChange = this.handleUsernameChange.bind(this);
+        this.handlePasswordChange = this.handlePasswordChange.bind(this);
+    }
+
+    attemptLogin(){
+        
+    }
+
+    handleUsernameChange(e){
+        this.setState({username:e.target.value});
+    }
+
+    handlePasswordChange(e){
+        this.setState({password:e.target.value});
+    }
+
+
     render() {
         console.log(this.state);
         return (
@@ -21,9 +44,9 @@ export default class LoginForm extends React.Component {
                     <Form>
                         <FormGroup>
                             <Label for="username">Username</Label>
-                            <Input type="text" id="username"/>
-                            <Label for="password">Password</Label>
-                            <Input type="password" id="password"/>
+                            <Input type="text" id="username" onChange={this.handleUsernameChange} value={this.state.username}/>
+                            <Label for="password" value={this.state.password}>Password</Label>
+                            <Input type="password" id="password"onChange={this.handlePasswordChange}/>
                             <Button>Login</Button>
                         </FormGroup>
                     </Form>
