727f61852f0c426d287db46afb9543c804dfbf32
ChrisChrisLoLo <sporewoh@gmail.com>
2019-04-23 07:55:25 -0700

    Added basic grid formatting

--- frontend/src/components/scheduleBuilder/ScheduleGrid.js
+++ frontend/src/components/scheduleBuilder/ScheduleGrid.js
@@ -4,14 +4,17 @@ import {
     Col
 } from 'reactstrap';
 import axios from "axios";
+import "./styles/ScheduleGrid.css";
 
 export default class ScheduleGrid extends React.Component {
     render() {
         console.log(this.state);
         return (
             <div>
-                <div>
-
+                <div className={"grid-container"}>
+                    <div className={"grid-test-1"}>
+                        HELLO
+                    </div>
                 </div>
             </div>
         );
