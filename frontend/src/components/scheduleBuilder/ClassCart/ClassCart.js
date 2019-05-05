6db61307a92f64fd7a32a104147a1575b676b468
ChrisChrisLoLo <sporewoh@gmail.com>
2019-05-05 14:25:50 -0600

    Added front-end pagination for the cart in the schedule builder. Need to iron out bugs with adding courses.

--- frontend/src/components/scheduleBuilder/ClassCart/ClassCart.js
+++ frontend/src/components/scheduleBuilder/ClassCart/ClassCart.js
@@ -1,5 +1,7 @@
 import React from "react";
 import {
+    Button,
+    ButtonGroup,
     Card,
     CardBody,
     CardHeader,
@@ -11,9 +13,12 @@ import getAuthToken from "../../../userLib/getAuthToken";
 
 
 export default class ClassCart extends React.Component {
+
     constructor(props) {
         super(props);
-        this.state={classesInCart:[]};
+        this.state={classesInCart:[],page:0};
+        this.handlePageChange = this.handlePageChange.bind(this);
+
     }
 
     componentDidUpdate(prevProps) {
@@ -32,27 +37,56 @@ export default class ClassCart extends React.Component {
         }
     }
 
+    handlePageChange(e,increment){
+        this.setState({page : this.state.page+=increment});
+    }
+
+
 
     render() {
-
+        const ITEMS_PER_PAGE = 5;
         const results = this.state.classesInCart;
-        console.log(results);
-        const cart = results.length>0?
-            results.map((classCart) =>
+
+        let cart;
+        if(results.length>0){
+            const paginatedResults = results.slice(this.state.page*ITEMS_PER_PAGE,(this.state.page*ITEMS_PER_PAGE)+ITEMS_PER_PAGE);
+            cart = paginatedResults.map((classCart) =>
                 <CartItem
                     key={classCart.id}
                     course={classCart.courseClass}
                     handleCourseClassAdd = {this.props.handleCourseClassAdd}
                     handleCourseClassRemove = {this.props.handleCourseClassRemove}
                 />
-            ):
-            <p>No Results Found</p>;
+            );
+        }
+        else{
+            cart = <p>No Results Found</p>;
+        }
+
+        let buttonGroup = null;
+        if(results.length>0){
+            buttonGroup = "HII";
+            buttonGroup =  (<ButtonGroup>
+                                <Button
+                                    size={"sm"}
+                                    disabled={this.state.page<=0}
+                                    onClick={(e)=>this.handlePageChange(e,-1)}
+                                >prev</Button>
+
+                                <Button
+                                    size={"sm"}
+                                    disabled={results.length-(this.state.page*ITEMS_PER_PAGE)<ITEMS_PER_PAGE}
+                                    onClick={(e)=>this.handlePageChange(e,1)}
+                                >next</Button>
+                            </ButtonGroup>);
+        }
 
         return (
             <Card>
                 <CardHeader><h5>Selected Courses:</h5></CardHeader>
                 <CardBody className={"px-1"}>
                     {cart}
+                    {buttonGroup}
                 </CardBody>
             </Card>
         );
