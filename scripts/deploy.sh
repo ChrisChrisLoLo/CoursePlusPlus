0c156d0d458a7f144a5e352ba5b22f76330df7cd
ChrisChrisLoLo <sporewoh@gmail.com>
2019-05-15 22:47:25 -0600

    Editing scripts to get deployment to be completely automatic

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,7 +3,7 @@
 npm run build --prefix ../frontend &&
 
 #Deploy frontend (transfer built frontend)
-scp -i "key.pem" -r ../frontend/build ubuntu@54.202.197.54:/var/www/CoursePlusPlus/UCourse/frontend
+scp -i "key.pem" -r ../frontend/build ubuntu@54.202.197.54:/var/www/CoursePlusPlus/frontend
 
 #Deploy backend (pull from github)
 ssh -i "key.pem" ubuntu@54.202.197.54 "cd /var/www/CoursePlusPlus && git pull"
--- scripts/runProd.sh
+++ scripts/runProd.sh
@@ -1,4 +1,14 @@
-cd /var/www/CoursePlusPlus/UCourse &&
-    pkill gunicorn
-	gunicorn --error-logfile /var/log/gunicorn/error.log -c gunicorn.py.ini UCourse.wsgi &
+cd /var/www/CoursePlusPlus/UCourse
+
+pkill gunicorn
+
+gunicorn --error-logfile /var/log/gunicorn/error.log -c gunicorn.py.ini UCourse.wsgi &
+
+#Let server boot up
+sleep 5
+
+#Check if running
+pgrep gunicorn && echo Running
+pgrep gunicorn || echo Not running
+
 cd /var/www/CoursePlusPlus/scripts
